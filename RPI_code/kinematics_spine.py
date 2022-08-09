026c817935ebfe25d94a75a5b03fca7665a86b72
clausc <ge93wit@mytum.de>
2022-08-09 13:05:09 +0200

    small change to calibarate

--- RPI_code/kinematics_spine.py
+++ RPI_code/kinematics_spine.py
@@ -1,8 +1,9 @@
-#import servo
+import servo
 import math
 
+
 def inverse_kinematics_spine(omega,theta,d,ofset,max_angle,spine_length,pully_radius,norma_rope_length):
-    if theta > 0:
+    if theta < 0:
         theta = 0
     elif theta > max_angle:
         theta = max_angle
@@ -16,18 +17,21 @@ def inverse_kinematics_spine(omega,theta,d,ofset,max_angle,spine_length,pully_ra
     for i in [0,1,2]:
         omega_i[i] = (math.pi/180)*(90+120*(i)-omega)
         li[i] = l - theta * d * math.cos(omega_i[i])
-
-    #clalculate angle that motor has to move as a delta vlaue from the normal length. with negative beein pulling rope inward
-    # the ofset must be tuned as such taht this works
+    #print(li)
+    #clalculate angle that motor has to move as a delta vlaue from the normal length. with postive beein pulling rope inward positive outward starting from the relaxed positon
+    # the ofset must be tuned as such that this works
     pully_circumference = pully_radius*2*math.pi
     pully_circumference_per_angle = pully_circumference/360
     angle = [0, 0, 0]
-    print(li)
-    angle[0] = (li[0]-norma_rope_length)/pully_circumference_per_angle
-    angle[1] = (li[1]-norma_rope_length)/pully_circumference_per_angle
-    angle[2] = (li[2]-norma_rope_length)/pully_circumference_per_angle
 
-    #set motor to that angle
-    servo.set_angle(angle[1], angle[2], angle[3], ofset)
+    #convert rope length to angle
+    li[0] = 0.2
+    angle[0] = -(li[0]-norma_rope_length)/pully_circumference_per_angle
+    angle[1] = -(li[1]-norma_rope_length)/pully_circumference_per_angle
+    angle[2] = -(li[2]-norma_rope_length)/pully_circumference_per_angle
+
+
+    #Move servo
+    servo.set_angle(angle[0], angle[1], angle[2],ofset)
     return angle
-print(inverse_kinematics_spine(90+120,20,0.07,[0,0,0],20,0.13,0.04,0.13))
+print(inverse_kinematics_spine(90+120,0,0.07,[-25,-25,-60],20,0.13,0.04,0.13))
--- RPI_code/servo.py
+++ RPI_code/servo.py
@@ -1,27 +1,36 @@
 
 import time
 from adafruit_servokit import ServoKit
-kit = ServoKit(channels=3)
+kit = ServoKit(channels=16)
 #inspired by https://learn.adafruit.com/16-channel-pwm-servo-driver/python-circuitpython
+
+def map(value, leftMin, leftMax, rightMin, rightMax):
+    left = leftMax - leftMin
+    right = rightMax - rightMin
+    scale = float(value - leftMin) / float(left)
+    return rightMin + (scale * right)
 
 def set_angle(servo_angle1, servo_angle2, servo_angle3,ofsets):
     #calculatring angles with the ofset
-    servo_angle1 = servo_angle1 - ofsets[0]
-    servo_angle2 = servo_angle2 - ofsets[1]
-    servo_angle3 = servo_angle3 - ofsets[2]
+    servo_angle1 = servo_angle1 +135 + ofsets[0] #+135 sets zero point of servo to center of ratiational maxima
+    servo_angle2 = servo_angle2 +135 + ofsets[1]
+    servo_angle3 = servo_angle3 + 135 + ofsets[2]
 
     # Setup:
-    servokit.servo[0].actuation_range = 180
-    servokit.servo[1].actuation_range = 180
-    servokit.servo[2].actuation_range = 180
-    kit.servo[0].set_pulse_width_range(800, 2000)
-    kit.servo[1].set_pulse_width_range(800, 2000)
-    kit.servo[2].set_pulse_width_range(800, 2000)
+    kit.servo[0].actuation_range = 270
+    kit.servo[1].actuation_range = 270
+    kit.servo[2].actuation_range = 270
+    kit.servo[0].set_pulse_width_range(470, 2550)
+    kit.servo[1].set_pulse_width_range(470, 2550)
+    kit.servo[2].set_pulse_width_range(470, 2550)
 
-    kit.servo[0].angle = servo_angle1
-    kit.servo[1].angle = servo_angle2
-    kit.servo[2].angle = servo_angle3
+    #map to invert
+    servo_angle1 = map(servo_angle1,0,270,270,0)
 
-set_angle(10,10,10,[0,0,0])
+    kit.servo[0].angle = servo_angle1 #rechts
+    kit.servo[1].angle = servo_angle2 #links
+    kit.servo[2].angle = servo_angle3#top
+
+#set_angle(10,10,10,[0,0,0])
 
 
